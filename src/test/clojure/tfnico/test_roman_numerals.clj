1d70580e830992608726cc79edb70bb9aa799a46
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-13 18:55:05 +0200

    Ten with a new cond

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -4,6 +4,7 @@
 (defn convert [number]
   (let [ mapping {0 "", 1 "I", 2 "II", 3 "III", 4 "IV", 5 "V"}]
     (cond
+      (= number 10) "X"
       (= number 9) "IX"
       (> number 5) (str "V" (convert (- number 5)))
       true (mapping number))))
@@ -19,6 +20,7 @@
     6 "VI"
     7 "VII"
     8 "VIII"
-    9 "IX"))
+    9 "IX"
+    10 "X"))
 
 (run-tests 'tfnico.test-roman-numerals)

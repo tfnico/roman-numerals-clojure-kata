fbe929ac90712393ead6cfc56e7947b934986441
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-13 16:32:17 +0200

    Refactor: Map lookup instead of nested if's

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -2,11 +2,8 @@
 	(:use clojure.test))
 
 (defn convert [number]
-  (if (= 0 number)
-    ""
-    (if (= 1 number)
-        "I"
-        "II")))
+  (let [ mapping {0 "", 1 "I", 2 "II"}]
+  (mapping number)))
 
 (deftest one
   (are [number numeral] (= (convert number) numeral)

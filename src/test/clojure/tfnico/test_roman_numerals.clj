bc4dbcc5abf95e497bfb19f4975bf69ab61ee527
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-13 14:30:24 +0200

    Number 1 becomes numeral 'I'

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -1,7 +1,11 @@
 (ns tfnico.test-roman-numerals
-	(:use clojure.test
-              tfnico.roman-numerals))
-	
+	(:use clojure.test))
+
+(defn convert [number]
+  "I")
+
 (deftest test-should-be-true
   (is (= "I" (convert 1))))
-	
+
+
+(run-tests 'tfnico.test-roman-numerals)

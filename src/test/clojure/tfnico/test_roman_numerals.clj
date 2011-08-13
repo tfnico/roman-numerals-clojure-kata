a9b32b348ba24232a39e2c20aae804825ebb2108
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-13 18:30:09 +0200

    Six to eight by iffing on larger than five, and append the rest

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -1,17 +1,22 @@
 (ns tfnico.test-roman-numerals
-	(:use clojure.test))
+ (:use clojure.test))
 
 (defn convert [number]
   (let [ mapping {0 "", 1 "I", 2 "II", 3 "III", 4 "IV", 5 "V"}]
-  (mapping number)))
+    (if(> number 5)
+      (str "V" (convert (- number 5)))
+      (mapping number))))
 
 (deftest one
   (are [number numeral] (= (convert number) numeral)
-                           0 ""
-                           1 "I"
-                           2 "II"
+    0 ""
+    1 "I"
+    2 "II"
     3 "III"
     4 "IV"
-    5 "V"))
+    5 "V"
+    6 "VI"
+    7 "VII"
+    8 "VIII"))
 
 (run-tests 'tfnico.test-roman-numerals)

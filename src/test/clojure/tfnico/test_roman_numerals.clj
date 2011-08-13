f404cc8a6f29eadeab4eb74ea25b5ff5db026a87
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-13 18:38:59 +0200

    Convert nine by nesting inner if (ugly)

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -3,9 +3,11 @@
 
 (defn convert [number]
   (let [ mapping {0 "", 1 "I", 2 "II", 3 "III", 4 "IV", 5 "V"}]
-    (if(> number 5)
-      (str "V" (convert (- number 5)))
-      (mapping number))))
+    (if(= number 9)
+      "IX"
+      (if(> number 5)
+        (str "V" (convert (- number 5)))
+        (mapping number)))))
 
 (deftest one
   (are [number numeral] (= (convert number) numeral)
@@ -17,6 +19,7 @@
     5 "V"
     6 "VI"
     7 "VII"
-    8 "VIII"))
+    8 "VIII"
+    9 "IX"))
 
 (run-tests 'tfnico.test-roman-numerals)

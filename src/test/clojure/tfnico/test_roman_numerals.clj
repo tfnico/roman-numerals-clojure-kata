5d86869b7a5cb866fc6f2ed6d8e47cf5177ed11f
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-13 19:00:15 +0200

    From ten to thirty-nine

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -4,7 +4,7 @@
 (defn convert [number]
   (let [ mapping {0 "", 1 "I", 2 "II", 3 "III", 4 "IV", 5 "V"}]
     (cond
-      (= number 10) "X"
+      (>= number 10) (str "X" (convert(- number 10)))
       (= number 9) "IX"
       (> number 5) (str "V" (convert (- number 5)))
       true (mapping number))))
@@ -21,6 +21,14 @@
     7 "VII"
     8 "VIII"
     9 "IX"
-    10 "X"))
+    10 "X"
+    11 "XI"
+    12 "XII"
+    14 "XIV"
+    15 "XV"
+    16 "XVI"
+    19 "XIX"
+    20 "XX"
+    39 "XXXIX"))
 
 (run-tests 'tfnico.test-roman-numerals)

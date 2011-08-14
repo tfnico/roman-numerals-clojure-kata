5963f67e77edbedad818832cea52ff69fc6661f4
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 18:35:31 +0200

    Convert 1 with if

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -2,10 +2,13 @@
 	(:use clojure.test))
 
 (defn convert [number]
-  "")
+  (if (= number 0)
+    ""
+    "I"))
 
 (deftest conversion
   (are [x y] (= (convert x) y)
-    0 ""))
+    0 ""
+    1 "I"))
 
 (run-tests 'tfnico.test-roman-numerals)

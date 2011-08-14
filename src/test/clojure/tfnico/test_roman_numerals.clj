db200e10ba9d8814cbcd6b46a0a5769f15a7b5c8
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 20:02:19 +0200

    Convert three with nested if's

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -6,12 +6,15 @@
     ""
     (if (= number 1)
       "I"
-      "II")))
+      (if (= number 2)
+        "II"
+        "III"))))
 
 (deftest conversion
   (are [x y] (= (convert x) y)
     0 ""
     1 "I"
-    2 "II"))
+    2 "II"
+    3 "III"))
 
 (run-tests 'tfnico.test-roman-numerals)

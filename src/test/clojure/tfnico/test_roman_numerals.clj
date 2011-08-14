83f015bf6880beaf2c80022df778124fc55a8b5b
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 18:33:54 +0200

    Zero. Use 'are'

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -1,7 +1,11 @@
 (ns tfnico.test-roman-numerals
 	(:use clojure.test))
-	
-(deftest one-larger-than-zero
-  (is (> 1 0)))
-	
+
+(defn convert [number]
+  "")
+
+(deftest conversion
+  (are [x y] (= (convert x) y)
+    0 ""))
+
 (run-tests 'tfnico.test-roman-numerals)

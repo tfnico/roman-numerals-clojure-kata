b3138eaf7648032ccd7448086dea364409920a9d
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 21:56:22 +0200

    Convert 1-3 with str

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -2,10 +2,16 @@
 	(:use clojure.test))
 
 (defn convert [number]
-  "")
+  (if (> number 0)
+    (str "I" (convert (- number 1)))
+    ""))
+
 
 (deftest conversion
   (are [x y] (= (convert x) y)
-    0 ""))
+    0 ""
+    1 "I"
+    2 "II"
+    3 "III"))
 
 (run-tests 'tfnico.test-roman-numerals)

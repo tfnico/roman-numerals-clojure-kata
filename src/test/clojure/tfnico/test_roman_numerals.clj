53d252d702c4c60493a9baba8369c62de865c2e7
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 14:16:48 +0200

    Zero or one with if

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -2,10 +2,15 @@
 	(:use clojure.test))
 
 (defn convert [number]
-  "")
+  (if (= 0 number) 
+    ""
+    "I"))
 
 
 (deftest zero
   (is (= "" (convert 0))))
+
+(deftest one
+  (is (= "I" (convert 1))))
 	
 (run-tests 'tfnico.test-roman-numerals)

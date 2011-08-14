42f67d63b739a9ddcac306d5eea5bfe8a4a4fd05
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 16:29:29 +0200

    Convert four, expanded map and defined to-use function

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -1,18 +1,24 @@
 (ns tfnico.test-roman-numerals
-	(:use clojure.test))
+ (:use clojure.test))
+
+(defn to-use [number numeralMap]
+  (if (= number 4 )
+    (first numeralMap)
+    (second numeralMap)))
 
 (defn convert [number]
-  (let [numeralMap {1 "I"}
-        entry (first numeralMap)]
-  (if (= 0 number) 
-    ""
-    (str (val entry) (convert (- number (key entry)))))))
+  (let [numeralMap {4 "IV", 1 "I"}
+        entry (to-use number numeralMap)]
+    (if (= 0 number)
+      ""
+      (str (val entry) (convert (- number (key entry)))))))
 
 (deftest convert-numeral
   (are [number numeral] (= (convert number) numeral)
     0 ""
     1 "I"
     2 "II"
-    3 "III"))
-	
+    3 "III"
+    4 "IV"))
+
 (run-tests 'tfnico.test-roman-numerals)

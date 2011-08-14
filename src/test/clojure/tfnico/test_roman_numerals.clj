1e62d25d339e231005dcbc660c1b4a14d2db2997
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 23:09:54 +0200

    Use a mapping to convert. Def to-use

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -1,10 +1,17 @@
 (ns tfnico.test-roman-numerals
 	(:use clojure.test))
 
+(defn to-use [number mapping]
+  (if (= number 4)
+    (first mapping)
+    (second mapping)))
+
 (defn convert [number]
+  (let [mapping {4 "IV", 1 "I"}
+        entry (to-use number mapping)]
   (if (> number 0)
-    (str "I" (convert (- number 1)))
-    ""))
+    (str (val entry) (convert (- number (key entry))))
+    "")))
 
 
 (deftest conversion
@@ -12,6 +19,7 @@
     0 ""
     1 "I"
     2 "II"
-    3 "III"))
+    3 "III"
+    4 "IV"))
 
 (run-tests 'tfnico.test-roman-numerals)

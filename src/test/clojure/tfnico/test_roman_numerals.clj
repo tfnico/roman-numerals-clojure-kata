b5239f701c918b98c1f6fd60f50c0e613a0113ad
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 20:50:33 +0200

    Convert 9 and 10 with nested if's

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -3,9 +3,14 @@
 
 (defn convert [number]
   (let [mappings {0 "", 1 "I", 2 "II", 3 "III",  4 "IV"}]
-    (if (>= number 5)
-      (str "V" (convert (- number 5)))
-        (mappings number))))
+    (if (= number 10)
+      "X"
+
+        (if (>= number 9)
+          "IX"
+            (if (>= number 5)
+              (str "V" (convert (- number 5)))
+                (mappings number))))))
 
 (deftest conversion
   (are [x y] (= (convert x) y)
@@ -15,6 +20,8 @@
     3 "III"
     4 "IV"
     5 "V"
-    6 "VI"))
+    6 "VI"
+    9 "IX"
+    10 "X"))
 
 (run-tests 'tfnico.test-roman-numerals)

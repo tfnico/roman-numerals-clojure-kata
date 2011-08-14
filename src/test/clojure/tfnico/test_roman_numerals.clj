09e36ed35ce01617f04ed3dabf6deda22dfa9085
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 19:59:44 +0200

    Convert two with nested if's

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -4,11 +4,14 @@
 (defn convert [number]
   (if (= number 0)
     ""
-    "I"))
+    (if (= number 1)
+      "I"
+      "II")))
 
 (deftest conversion
   (are [x y] (= (convert x) y)
     0 ""
-    1 "I"))
+    1 "I"
+    2 "II"))
 
 (run-tests 'tfnico.test-roman-numerals)

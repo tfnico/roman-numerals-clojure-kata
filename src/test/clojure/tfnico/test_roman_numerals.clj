8ec8b91449f36d4216cf26e83ed71f13613ec20e
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 21:01:07 +0200

    Convert from 10 to 39 with str

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -4,7 +4,7 @@
 (defn convert [number]
   (let [mappings {0 "", 1 "I", 2 "II", 3 "III",  4 "IV"}]
     (cond
-      (= number 10) "X"
+      (>= number 10) (str "X" (convert (- number 10)))
       (= number 9) "IX"
       (>= number 5) (str "V" (convert (- number 5)))
       true (mappings number))))
@@ -19,6 +19,11 @@
     5 "V"
     6 "VI"
     9 "IX"
-    10 "X"))
+    10 "X"
+    11 "XI"
+    14 "XIV"
+    19 "XIX"
+    25 "XXV"
+    39 "XXXIX"))
 
 (run-tests 'tfnico.test-roman-numerals)

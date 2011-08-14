07f2b0163a2de385bda530142a3146cf27f82102
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-14 14:05:30 +0200

    Zero is empty

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -1,7 +1,11 @@
 (ns tfnico.test-roman-numerals
 	(:use clojure.test))
-	
-(deftest one-larger-than-zero
-  (is (> 1 0)))
+
+(defn convert [number]
+  "")
+
+
+(deftest zero
+  (is (= "" (convert 0))))
 	
 (run-tests 'tfnico.test-roman-numerals)

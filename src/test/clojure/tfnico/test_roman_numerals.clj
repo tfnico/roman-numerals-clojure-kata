8ba12250e1cc4c9db8cf6560a8252b6d9961e8ba
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-15 19:41:59 +0200

    Convert 0

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -1,7 +1,10 @@
 (ns tfnico.test-roman-numerals
 	(:use clojure.test))
+
+(defn convert [number]
+  "")
 	
 (deftest test-convert
-  (is (= 0 0)))
+  (is (= "" (convert 0))))
 	
 (run-tests 'tfnico.test-roman-numerals)

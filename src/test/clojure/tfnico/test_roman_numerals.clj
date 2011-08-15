93814236a119e06b2f20a084efa9096871b9c1a9
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-15 18:35:28 +0200

    Convert 4 to 7 using map lookup

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -2,7 +2,7 @@
 	(:use clojure.test))
 
 (defn convert [number]
-  (let [mapping {0 "", 1 "I", 2 "II", 3 "III"}]
+  (let [mapping {0 "", 1 "I", 2 "II", 3 "III", 4 "IV", 5 "V", 6 "VI", 7 "VII"}]
     (mapping number)))
 
 (deftest test-convert
@@ -10,6 +10,10 @@
     0 ""
     1 "I"
     2 "II"
-    3 "III"))
+    3 "III"
+    4 "IV"
+    5 "V"
+    6 "VI"
+    7 "VII"))
 	
 (run-tests 'tfnico.test-roman-numerals)

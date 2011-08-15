ae89e3ad5be12cd83550d50059ff97a86a33ce6b
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-15 19:56:48 +0200

    Convert 10 with if

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -3,11 +3,13 @@
 
 (defn convert [number]
   (let [mapping {0 "", 1 "I", 2 "II", 3 "III", 4 "IV"}]
-    (if (= number 9) 
+    (if (= number 10)
+      "X"
+       (if (= number 9)
         "IX"
         (if (>= number 5)
           (str "V" (convert (- number 5)))
-          (mapping number)))))
+          (mapping number))))))
            
 	
 (deftest test-convert
@@ -21,7 +23,8 @@
     6 "VI"
     7 "VII"
     8 "VIII"
-    9 "IX"))
+    9 "IX"
+    10 "X"))
 
 
 

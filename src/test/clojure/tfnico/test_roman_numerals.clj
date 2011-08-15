051fab1b1bf1a261008a10fe37650b839aad81e5
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-15 18:14:03 +0200

    Get ready: test-convert

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -1,7 +1,7 @@
 (ns tfnico.test-roman-numerals
 	(:use clojure.test))
 	
-(deftest one-larger-than-zero
-  (is (> 1 0)))
+(deftest test-convert
+  (is (= 0 0)))
 	
 (run-tests 'tfnico.test-roman-numerals)

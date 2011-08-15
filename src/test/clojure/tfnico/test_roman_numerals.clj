25d85909bd00f257abb2f3a9995aa542fab5c08b
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2011-08-15 18:40:04 +0200

    Refactor: Concat recurively from 5

--- src/test/clojure/tfnico/test_roman_numerals.clj
+++ src/test/clojure/tfnico/test_roman_numerals.clj
@@ -2,8 +2,10 @@
 	(:use clojure.test))
 
 (defn convert [number]
-  (let [mapping {0 "", 1 "I", 2 "II", 3 "III", 4 "IV", 5 "V", 6 "VI", 7 "VII"}]
-    (mapping number)))
+  (let [mapping {0 "", 1 "I", 2 "II", 3 "III", 4 "IV"}]
+    (if (>= number 5)
+      (str "V" (convert (- number 5)))
+      (mapping number))))
 
 (deftest test-convert
   (are [x y ] (= y (convert x))
